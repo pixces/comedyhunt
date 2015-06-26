da6c3fa405e005246c2b642039327c9f3196efb7
Zainul <zaabdeen@souq.com>
2015-06-26 22:08:11 +0530

    adding config

---
+++ protected/config/dev.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: zainulabdeen
+ * Date: 29/01/14
+ * Time: 9:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+return CMap::mergeArray(
+    require(dirname(__FILE__) . '/main.php'),
+    array(
+        'components' => array(
+            'db' => array(
+                'connectionString' => 'mysql:host=localhost;dbname=comedyhunt',
+                'emulatePrepare' => true,
+                'username' => 'root',
+                'password' => 'root',
+                'charset' => 'utf8',
+                'enableParamLogging' => true,
+            ),
+        ),
+        'params' => array(
+            's3AccessKey' => "AKIAI4PTFQAGUMKSMN3Q",
+            's3SecretKey' => "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",
+            's3Bucket' => "cnkugc",
+        ),
+        'modules' => array(
+            'admin',
+            'gii' => array(
+                'class' => 'system.gii.GiiModule',
+                'password' => 'position2',
+                'ipFilters' => array('127.0.0.1', '::1'),
+            ),
+        ),
+        /*
+        'log'=>array(
+            'class'=>'CLogRouter',
+            'routes'=>array(
+                array(
+                    'class'=>'CFileLogRoute',
+                    'levels'=>'error, warning,trace, info',
+                ),
+                // uncomment the following to show log messages on web pages
+                array(
+                    'class' => 'CWebLogRoute',
+                    'enabled' => YII_DEBUG,
+                    'levels' => 'error, warning, trace, notice',
+                    'categories' => 'application',
+                    'showInFireBug' => false,
+                ),
+            ),
+        ),*/
+    )
+);
