f616f54e11834bb3ca9dbd11e5b2eefeecfa2478
Zainul <zaabdeen@souq.com>
2015-06-27 22:55:06 +0530

    merged updated

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function(){
     $("#CH-VideoCarousel").owlCarousel({
         //autoPlay: 3000, //Set AutoPlay to 3 seconds
-        items: 4,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3],
-        itemsMobile: [600, 1],
-        navigation: true
+        items:4,
+        itemsDesktop:[1199,4],
+        itemsDesktopSmall:[979,3],
+        itemsMobile:[600,1],
+        navigation:true
     });
-    $("#CH-MobileMenu").click(function() {
+    $("#CH-MobileMenu").click(function(){
         $(this).next().slideToggle();
     });
 
     // function to show our popups
-    function showPopup(whichpopup) {
+    function showPopup(whichpopup){
         var docHeight = $(document).height();
         var scrollTop = $(window).scrollTop();
-        $('.overlay-wrapper').show().css({'height': docHeight});
-        $('.popup' + whichpopup).show();
+        $('.overlay-wrapper').show().css({'height' : docHeight});
+        $('.popup'+whichpopup).show();
     }
 
     // function to close our popups
-    function closePopup() {
+    function closePopup(){
         $('.overlay-wrapper, .overlay-content').hide(); //hide the overlay
     }
 
-    $('.show-popup').on('click', function(event) {
+    $('.show-popup').on('click',function(event){
         event.preventDefault();
         var selectedPopup = $(this).data('showpopup');
         showPopup(selectedPopup);
@@ -32,14 +32,14 @@ $(document).ready(function() {
         var VideoURL = $(this).attr('data-videoURL');
         var VideoTitle = $(this).attr('data-videoTitle');
 
-        var iframeTemplate = '<iframe width="853" height="480" src="https://www.youtube.com/embed/' + VideoURL + '?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;modestbranding=1;enablejsapi=1" frameborder="0" allowfullscreen id="th-553fed94be2f27b50100729a-video"></iframe>';
+        var iframeTemplate = '<iframe width="853" height="480" src="https://www.youtube.com/embed/'+VideoURL+'?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;modestbranding=1;enablejsapi=1" frameborder="0" allowfullscreen id="th-553fed94be2f27b50100729a-video"></iframe>';
 
         $(".overlay-content .overlay-title").html(VideoTitle);
         $(".overlay-content .modalVideo").html(iframeTemplate);
     });
 
     // hide popup when user clicks on close button or if user clicks anywhere outside the container
-    $('.close-btn, .overlay-wrapper').on('click', function() {
+    $('.close-btn, .overlay-wrapper').on('click',function(){
         $(".overlay-content .overlay-title").empty();
         $(".overlay-content .modalVideo").empty();
         closePopup();
@@ -55,85 +55,71 @@ $(document).ready(function() {
     /*$('.CH-Video .CH-VideoThumbPlay a').on('click',function(event){
      var CHVideoURL = $(this).attr('data-CHVideoURL');
      var CHVideoTitle = $(this).attr('data-CHVideoTitle');
-     
+
      var iframeTemplate1 = '<iframe width="853" height="480" src="https://www.youtube.com/embed/'+CHVideoURL+'?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;modestbranding=1;enablejsapi=1" frameborder="0" allowfullscreen id="th-553fed94be2f27b50100729a-video"></iframe>';
-     
+
      $(".CH-VideoOverlay .CH-VideoTitle").html(CHVideoTitle);
      $(".CH-VideoOverlay .CH-VideoDescription").html(iframeTemplate1);
      $(".CH-VideoOverlay").show();
      });
-     
+
      $('.CH-VideoClose').on('click',function(){
      $(".CH-VideoOverlay .CH-VideoTitle").empty();
      $(".CH-VideoOverlay .CH-VideoDescription").empty();
      $(".CH-VideoOverlay").hide();
      });*/
 
-    $(window).resize(function() {
+    $(window).resize(function(){
         var windowWidth = $(window).width();
-        if (windowWidth > 767) {
+        if(windowWidth > 767){
             $(".CH-MainMenu ul").show();
-        } else {
+        }else{
             $(".CH-MainMenu ul").hide();
         }
     });
-    /**
-     * 
-     * @type @call;$
-     */
-    var jScrollContainer = $('.CH-GalleryList .scroll-pane');
 
-    /**
-     * jSCROLLPANE ON SCROLL
-     */
-    jScrollContainer
-
-            .bind(
-                    'jsp-scroll-y',
-                    function(event, scrollPositionY, isAtTop, isAtBottom)
-                    {
-//					console.log('Handle jsp-scroll-y', this,
-//								'scrollPositionY=', scrollPositionY,
-//								'isAtTop=', isAtTop,
-//								'isAtBottom=', isAtBottom);
-                        if (!isAtBottom && scrollPositionY != 0) {
-
-                            var loadCounter = $("#loadCounter").val();
-                            if (globalLoadCounter !== loadCounter) {
-                                loadCounter = parseInt(loadCounter) + 1;
-                                var data = 'page=' + loadCounter + '&isAjaxRequest=1';
-                                $.ajax({
-                                    type: 'POST',
-                                    dataType: 'json',
-                                    url: HOST + '/gallery/index',
-                                    data: data,
-                                    success: function(data) {
-                                        if (data.count > 0) {
-                                            $("#gallerieVideoContent").html(data.content);
-                                            $("#loadCounter").val(loadCounter);
-                                        }
-                                        
-                                    }, error: function(xhr, status, error,data) {
-                                        console.log(data);
-                                        // $('#ImgError').html(data.error);
-
-                                    }
-                                });
-
-                            }
-
-                        }
-
-                    }
-            )
-
-
-            .jScrollPane();
-
-    jScrollContainer.jScrollPane({
-        autoReinitialise: true
+    $('.CH-GalleryList .scroll-pane').jScrollPane({
+        autoReinitialise:true
     });
+
+    $('.CH-FaqList .scroll-pane').jScrollPane({
+        autoReinitialise:true
+    });
+
+    $('.CH-RulesList .scroll-pane').jScrollPane({
+        autoReinitialise:true
+    });
+
+    $(".videoThumb4").ytplaylist({
+        holderId: 'ytvideo4',
+        html5: true,
+        playerWidth: '520',
+        autoPlay: true,
+        sliding: false,
+        listsliding: true,
+        slidingshow: true,
+        social: true,
+        autoHide: false,
+        playfirst: 0,
+        playOnLoad: false,
+        modestbranding: true,
+        showInfo: false
+    });
+
+    $(".videoThumb3").ytplaylist({
+        holderId: 'ytvideo3',
+        html5: true,
+        playerWidth: '520',
+        autoPlay: true,
+        sliding: false,
+        listsliding: true,
+        slidingshow: true,
+        social: true,
+        autoHide: false,
+        playfirst: 0,
+        playOnLoad: false,
+        modestbranding: true,
+        showInfo: false
+    });
+
 });
-
-
-
---
+++ protected/views/layouts/default.php
@@ -0,0 +1,34 @@
+<?php /* @var $this Controller */ ?>
+<?php $this->beginContent('//layouts/main'); ?>
+
+    <!-- Content Starts !-->
+    <?php echo $content; ?>
+    <!-- Content Ends !-->
+
+    <!-- TimeLine Starts Here -->
+    <div class="CH-TimeLine">
+        <div class="CH-TimeLineBG"></div>
+        <div class="CH-CompetitionTimeline">
+            <div class="CH-TimelineContent">
+                <span class="CH-Head">Your Comedy <br/>Calendar</span>
+                <span class="CH-Description">This fight for the funniest is split into three phases.</span>
+            </div>
+            <div class="CH-TimelineRules">
+                <div class="CH-TimelineRulesImage"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/competition-timeline.png" class="img-responsive" /></div>
+                <div class="CH-Phase1">
+                    <span class="CH-Title">Audition Phase</span>
+                    <span class="CH-Description">We’re accepting and reviewing all submissions before the 21st of July</b></span>
+                </div>
+                <div class="CH-Phase2">
+                    <span class="CH-Title">Challenge Phase</span>
+                    <span class="CH-Description">Once our judges pick their top talents, challenges are set every week. Follow the funny here.</span>
+                </div>
+                <div class="CH-Phase3">
+                    <span class="CH-Title">Finale</span>
+                    <span class="CH-Description">This is it, the big time. After the challenges are complete, the top five contestants will perform live with their comedy coaches. Stay tuned for more info on how the finale is shaping up...</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- TimeLine Ends Here -->
+<?php $this->endContent(); ?>
