4b2e6ec8e98e3eac238ac34a8e26b99142f1abca
unknown <Vikram.Jayaprakash@HMECL000468.happiestminds.com>
2015-07-07 01:49:24 +0530

    Contest Entries added in index page

--- protected/views/pages/index.php
+++ protected/views/pages/index.php
@@ -80,6 +80,50 @@
 </div>
 <!-- Submissions Ends Here -->
 
+<!-- Contest Entries Starts Here -->
+<div class="CH-ContestEntries CH-HomeFaq">
+	<div class="CH-ContestEntriesContent">
+		<div class="CH-ContestEntriesContentIcon"><img src="images/gallery-image-blue.png"/></div>
+		<div class="CH-ContestEntriesContentHead">Contest Entries</div>
+		<!-- dynamic playlist start -->
+		<?php foreach($aVideoList as $objVideo) {
+		?>
+		<div class="CH-FaqList">
+			<!-- Needed for the youtube player example 3 -->
+			<div class="youtubeplayer">
+				<div class="yt_holder yt_holder_right">
+					<div id="ytContestEntries" class="ytvideo"></div>
+					<!--Up and Down arrow -->
+					<div class="you_up"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/up_arrow.png" alt="+ Slide" title="HIDE" /></div>
+					<div class="you_down"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/down_arrow.png" alt="- Slide" title="SHOW" /></div>
+					<!-- END  -->
+					<div class="youplayer ytplayerright">
+						<ul class="videoyou videoytright scroll-pane">
+							<?php
+							if ( $objVideo->get_videos() !=null ) {
+								foreach ($objVideo->get_videos() as $yKey => $yValue) {
+									echo '<li><p>' . $yValue['title'] . '</p><a class="ContestEntriesThumb" href="http://www.youtube.com/watch?v=' . $yValue['videoid'] . '">' . $yValue['description'] . '</a></li>';
+								}
+							}else{
+								echo '<li>Sorry, no video\'s found</li>';
+							}
+							?>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- END youtube player -->
+		</div>
+		<?php } ?>
+		<!-- dynamic playlist ends -->
+		
+		<div class="CH-HomeEmbedList">
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/lIsIOsXkID4?list=PLX19kCCkfm1pR0fml9KXZzur6FVU5BiFO" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>
+</div>
+<!-- Contest Entries Ends Here -->
+
 <!-- Videos Carousel Starts Here -->
 <div class="CH-VideoCarousel">
     <div class="CH-VideoCarouselBG"></div>
