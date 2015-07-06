e4fb37aca3e9974197f88b37a8e0e35ff1a35050
unknown <Vikram.Jayaprakash@HMECL000468.happiestminds.com>
2015-07-07 01:52:31 +0530

    contest entries js file

--- js/main.js
+++ js/main.js
@@ -121,6 +121,22 @@ $(document).ready(function(){
 		autoReinitialise:true
 	});
 	
+	$(".ContestEntriesThumb").ytplaylist({
+		holderId: 'ytContestEntries',
+		html5: true,
+		playerWidth: '520',
+		autoPlay: true,
+		sliding: false,
+		listsliding: true,
+		slidingshow: true,
+		social: true,
+		autoHide: false,
+		playfirst: 0,
+		playOnLoad: false,
+		modestbranding: true,
+		showInfo: false
+	});
+	
 	$(".videoThumb4").ytplaylist({
 		holderId: 'ytvideo4',
 		html5: true,
