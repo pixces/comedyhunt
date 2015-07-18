bd4d735a2fd1af4adaa9d47f90d8f819c30ae043
unknown <Vikram.Jayaprakash@HMECL000468.happiestminds.com>
2015-07-18 18:37:50 +0530

    Gallery changes are done

--- protected/views/gallery/index.php
+++ protected/views/gallery/index.php
@@ -19,7 +19,7 @@ Gallery Ends Here -->
     <div class="CH-GalleryBG"></div>
     <div class="CH-GalleryContent">
         <div class="CH-GalleryIcon"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/gallery-image.png" /></div>
-        <div class="CH-GalleryHead">Entries so far...</div>
+        <div class="CH-GalleryHead">Entry Video #61 to 90...</div>
         <div class="CH-GalleryList">
 		
             <!--div class="scroll-pane">
@@ -29,13 +29,15 @@ Gallery Ends Here -->
             <!--</div>-->
 			
 			<!-- dynamic playlist start -->
-			<?php $t = 1; foreach($aVideoList as $objVideo) {
+			<?php /*$t = 1; foreach($aVideoList as $objVideo) {*/	?>
+			<?php if ($aVideoList[0]) {
+                $objVideo = $aVideoList[0];
 			?>
 			<div class="">
 				<!-- Needed for the youtube player example 3 -->
 				<div class="youtubeplayer">
 					<div class="yt_holder yt_holder_right">
-						<div id="ytvideogallery<?php echo $t; ?>" class="ytvideo"></div>
+						<div id="ytvideogallery1<?php /*echo $t;*/ ?>" class="ytvideo"></div>
 						<!--Up and Down arrow -->
 						<div class="you_up"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/up_arrow.png" alt="+ Slide" title="HIDE" /></div>
 						<div class="you_down"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/down_arrow.png" alt="- Slide" title="SHOW" /></div>
@@ -45,8 +47,10 @@ Gallery Ends Here -->
 								<?php
 								if ( $objVideo->get_videos() !=null ) {
 									foreach ($objVideo->get_videos() as $yKey => $yValue) {
-										echo '<li><p>' . $yValue['title'] . '</p><a class="videoThumbgallery'. $t .'" href="http://www.youtube.com/watch?v=' . $yValue['videoid'] . '">' . $yValue['description'] . '</a></li>';
+										echo '<li><p>' . $yValue['title'] . '</p><a class="videoThumbgallery1" href="http://www.youtube.com/watch?v=' . $yValue['videoid'] . '">' . $yValue['description'] . '</a></li>';
 									}
+									
+									//'. $t .'
 								}else{
 									echo '<li>Sorry, no video\'s found</li>';
 								}
@@ -57,9 +61,77 @@ Gallery Ends Here -->
 				</div>
 				<!-- END youtube player -->
 			</div>
-			<?php $t++; } ?>
+			<?php /*$t++; }*/ ?>
+			<?php } ?>
 			<!-- dynamic playlist ends -->
         </div>
+		
+		
+		<!-- Please UnComment below code to enable another playlist and add playlist in production.php under gallery section -->
+		<div class="CH-GalleryIcon"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/gallery-image.png" /></div>
+        <div class="CH-GalleryHead">Entry Video #31 to 60...</div>
+        <div class="CH-GalleryList">		
+			<?php if ($aVideoList[1]) { $objVideo = $aVideoList[1]; ?>
+			<div class="">
+				<div class="youtubeplayer">
+					<div class="yt_holder yt_holder_right">
+						<div id="ytvideogallery2" class="ytvideo"></div>
+						
+						<div class="you_up"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/up_arrow.png" alt="+ Slide" title="HIDE" /></div>
+						<div class="you_down"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/down_arrow.png" alt="- Slide" title="SHOW" /></div>
+						
+						<div class="youplayer ytplayerright">
+							<ul class="videoyou videoytright scroll-pane">
+								<?php
+								if ( $objVideo->get_videos() !=null ) {
+									foreach ($objVideo->get_videos() as $yKey => $yValue) {
+										echo '<li><p>' . $yValue['title'] . '</p><a class="videoThumbgallery2" href="http://www.youtube.com/watch?v=' . $yValue['videoid'] . '">' . $yValue['description'] . '</a></li>';
+									}
+								}else{
+									echo '<li>Sorry, no video\'s found</li>';
+								}
+								?>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php } ?>
+        </div>
+		
+		<!-- Please UnComment below code to enable another playlist and add playlist in production.php under gallery section -->
+		<div class="CH-GalleryIcon"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/gallery-image.png" /></div>
+        <div class="CH-GalleryHead">Entry Video #1 to 30</div>
+        <div class="CH-GalleryList">		
+			<?php if ($aVideoList[2]) { $objVideo = $aVideoList[2]; ?>
+			<div class="">
+				<div class="youtubeplayer">
+					<div class="yt_holder yt_holder_right">
+						<div id="ytvideogallery3" class="ytvideo"></div>
+						
+						<div class="you_up"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/up_arrow.png" alt="+ Slide" title="HIDE" /></div>
+						<div class="you_down"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/down_arrow.png" alt="- Slide" title="SHOW" /></div>
+						
+						<div class="youplayer ytplayerright">
+							<ul class="videoyou videoytright scroll-pane">
+								<?php
+								if ( $objVideo->get_videos() !=null ) {
+									foreach ($objVideo->get_videos() as $yKey => $yValue) {
+										echo '<li><p>' . $yValue['title'] . '</p><a class="videoThumbgallery3" href="http://www.youtube.com/watch?v=' . $yValue['videoid'] . '">' . $yValue['description'] . '</a></li>';
+									}
+								}else{
+									echo '<li>Sorry, no video\'s found</li>';
+								}
+								?>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php } ?>
+        </div>
+		
+		
     </div>
 </div>
 <!-- Gallery Ends Here -->
